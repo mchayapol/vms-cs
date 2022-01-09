cdf182f2ec88fbea7c71ff72c25a8a4681d05c93
Chayapol Moemeng <mchayapol@gmail.com>
2022-01-09 09:06:58 +0700

    Styled top news

--- src/components/header-news.js
+++ src/components/header-news.js
@@ -1,6 +1,10 @@
+/*
+Show at most 5 news
+TODO: It is hardcoded for 4 top news.
+*/
 import React, { useState } from "react";
 
-const jumboNewsList = [
+const topNewsList = [
   {
     title: "Software Engineering",
     featuredImage: "https://www.cs.au.edu/assets/img/IMG_4117.jpg",
@@ -33,61 +37,166 @@ const jumboNewsList = [
 
 const HeaderNews = ({ children }) => {
   const [currentNewsIndex, setCurrentNewsIndex] = useState(0);
-  const [news, setNews] = useState(jumboNewsList[0]);
+  const [news, setNews] = useState(topNewsList[0]);
   const selectNews = (idx) => {
-    setNews(jumboNewsList[idx]);
+    setNews(topNewsList[idx]);
     setCurrentNewsIndex(idx);
   };
 
   return (
-    <div className="bg-white dark:bg-gray-800 ">
-      <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
-        <div className="flex justify-center order-1 mr-0 mt-6 md:mt-0 md:space-y-3 md:flex-col">
-          {jumboNewsList.map((news, i) => {
-            const activeButton =
-              "w-3 h-3 mx-2 bg-cs-orange rounded-full md:mx-0 focus:outline-none";
-            const regularButton =
-              "w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500";
-            return (
-              <button
-                key={i}
-                className={
-                  currentNewsIndex === i ? activeButton : regularButton
-                }
-                onClick={() => selectNews(i)}
-              ></button>
-            );
-          })}
-        </div>
+    <div className="bg-white dark:bg-gray-800">
+      <div className="sp-slideshow">
+        <input
+          id="button-1"
+          type="radio"
+          name="radio-set"
+          className="sp-selector-1"
+          defaultChecked={true}
+        />
+        <label htmlFor="button-1" className="button-label-1"></label>
 
-        <div
-          key={news.title}
-          className={`flex flex-col items-center w-full md:flex-row order-2 lg:animate-fadein`}
-        >
-          <div className="w-xs md:max-w-full md:mx-12 md:order-2">
-            <h1 className="text-3xl font-medium tracking-wide text-cs-orange md:text-4xl">
-              {news.title}
-            </h1>
-            <p className="mt-4 text-neutral-50">{news.content}</p>
-            <div className="mt-6">
-              <a
-                href={news.url}
-                className="block bg-cs-orange px-3 py-2 font-semibold text-center text-white transform bg-cs-yellow rounded-md md:inline hover:bg-auspark-red"
-              >
-                Read more
-              </a>
-            </div>
-          </div>
-          <div className="flex items-center justify-center order-3  h-0 w-0  lg:w-full lg:h-full ">
-            <img
-              className="object-cover h-fit w-fit invisible md:visible rounded-md"
-              src={news.featuredImage}
-              alt={news.title}
-            />
-          </div>
+        <input
+          id="button-2"
+          type="radio"
+          name="radio-set"
+          className="sp-selector-2"
+        />
+        <label htmlFor="button-2" className="button-label-2"></label>
+
+        <input
+          id="button-3"
+          type="radio"
+          name="radio-set"
+          className="sp-selector-3"
+        />
+        <label htmlFor="button-3" className="button-label-3"></label>
+
+        <input
+          id="button-4"
+          type="radio"
+          name="radio-set"
+          className="sp-selector-4"
+        />
+        <label htmlFor="button-4" className="button-label-4"></label>
+
+        {/* <input
+          id="button-5"
+          type="radio"
+          name="radio-set"
+          className="sp-selector-5"
+        />
+        <label htmlFor="button-5" className="button-label-5"></label> */}
+
+        <label htmlFor="button-1" className="sp-arrow sp-a1"></label>
+        <label htmlFor="button-2" className="sp-arrow sp-a2"></label>
+        <label htmlFor="button-3" className="sp-arrow sp-a3"></label>
+        <label htmlFor="button-4" className="sp-arrow sp-a4"></label>
+        {/* <label htmlFor="button-5" className="sp-arrow sp-a5"></label> */}
+
+        <div className="sp-content">
+          <div className="sp-parallax-bg"></div>
+          <ul className="sp-slider clearfix">
+            <li>
+              <News news={topNewsList[0]} />
+            </li>
+            <li>
+            <News news={topNewsList[1]} />
+            </li>
+            <li>
+            <News news={topNewsList[2]} />
+            </li>
+            <li>
+            <News news={topNewsList[3]} />
+            </li>
+            {/* <li>
+              <img
+                src="https://tympanus.net/Tutorials/CSS3FluidParallaxSlideshow/images/image5.png"
+                alt="image05"
+              />
+            </li> */}
+          </ul>
         </div>
       </div>
+
     </div>
   );
 };
+
+const News = ({news}) => (
+  <div
+    key={news.title}
+    className={`flex flex-col items-center w-full md:flex-row order-2`}
+  >
+    <div className="w-xs md:max-w-full md:mx-12 md:order-2">
+      <h1 className="text-3xl font-medium tracking-wide text-cs-orange md:text-4xl">
+        {news.title}
+      </h1>
+      <p className="mt-4 text-neutral-50">{news.content}</p>
+      <div className="mt-6">
+        <a
+          href={news.url}
+          className="block bg-cs-orange px-3 py-2 font-semibold text-center text-white transform bg-cs-yellow rounded-md md:inline hover:bg-auspark-red"
+        >
+          Read more
+        </a>
+      </div>
+    </div>
+    <div className="flex items-center justify-center order-3  h-0 w-0  lg:w-full lg:h-full ">
+      <img
+        className="object-cover h-fit w-fit invisible md:visible rounded-md"
+        src={news.featuredImage}
+        alt={news.title}
+      />
+    </div>
+  </div>
+);
 export default HeaderNews;
+
+
+
+{/* <div className="container flex flex-col px-6 py-4 mx-auto space-y-6 md:h-128 md:py-16 md:flex-row md:items-center md:space-x-6">
+<div className="flex justify-center order-1 mr-0 mt-6 md:mt-0 md:space-y-3 md:flex-col">
+  {jumboNewsList.map((news, i) => {
+    const activeButton =
+      "w-3 h-3 mx-2 bg-cs-orange rounded-full md:mx-0 focus:outline-none";
+    const regularButton =
+      "w-3 h-3 mx-2 bg-gray-300 rounded-full md:mx-0 focus:outline-none hover:bg-blue-500";
+    return (
+      <button
+        key={i}
+        className={
+          currentNewsIndex === i ? activeButton : regularButton
+        }
+        onClick={() => selectNews(i)}
+      ></button>
+    );
+  })}
+</div>
+
+<div
+  key={news.title}
+  className={`flex flex-col items-center w-full md:flex-row order-2 lg:animate-fadein`}
+>
+  <div className="w-xs md:max-w-full md:mx-12 md:order-2">
+    <h1 className="text-3xl font-medium tracking-wide text-cs-orange md:text-4xl">
+      {news.title}
+    </h1>
+    <p className="mt-4 text-neutral-50">{news.content}</p>
+    <div className="mt-6">
+      <a
+        href={news.url}
+        className="block bg-cs-orange px-3 py-2 font-semibold text-center text-white transform bg-cs-yellow rounded-md md:inline hover:bg-auspark-red"
+      >
+        Read more
+      </a>
+    </div>
+  </div>
+  <div className="flex items-center justify-center order-3  h-0 w-0  lg:w-full lg:h-full ">
+    <img
+      className="object-cover h-fit w-fit invisible md:visible rounded-md"
+      src={news.featuredImage}
+      alt={news.title}
+    />
+  </div>
+</div>
+</div> */}
