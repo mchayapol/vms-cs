96b0af9e81c06e1daab8adbf71960fd77d504cd8
Chayapol Moemeng <mchayapol@gmail.com>
2022-01-10 17:54:36 +0700

    Added helmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,32 @@
 import * as React from "react";
 import Header from "../components/header";
 import Footer from "./footer";
+import { Helmet } from "react-helmet";
+
 const Layout = ({ children }) => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>VMS Computer Science</title>
+        <link rel="canonical" href="https://www.cs.au.edu" />
+
+        <script type="application/ld+json">
+          {`
+        {
+          "@context": "https://schema.org",
+          "@type": "Study Program",
+          "url": "https://www.cs.au.edu",
+          "name": "Computer Science",
+          "contactPoint": {
+            "@type": "ContactPoint",
+            "telephone": "+66-2-723-2380
+            "contactType": "Customer Support"
+          }
+        }
+      `}
+        </script>
+      </Helmet>
       <Header />
       <main>{children}</main>
       <Footer />
