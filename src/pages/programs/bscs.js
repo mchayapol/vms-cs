7ad9d2ea9ca6a5e14cf2c04d591b964fd8d430de
Chayapol Moemeng <mchayapol@gmail.com>
2022-01-10 14:32:12 +0700

    Styled MSCS

--- src/pages/programs/bscs.js
+++ src/pages/programs/bscs.js
@@ -15,9 +15,9 @@ const BsProgramPage = () => {
             <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg shadow-gray-600">
               <RandomImage className=" w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg" />
               <div className="p-6 flex flex-col justify-start">
-                <h5 className="text-gray-900 text-xl font-medium mb-2">
+                <h1 className="text-gray-900 text-xl font-medium mb-2">
                   BSCS Highlights
-                </h5>
+                </h1>
                 <p className="text-gray-700 text-base mb-4">
                   <ul>
                     <li>Internationally competant</li>
